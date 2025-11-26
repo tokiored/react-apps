b6423e64722ae5339f8b8bf6fb9a25a7df1038a1
tokiored <jason@tokiored.com>
2025-11-26 15:30:49 +0000

    initilise app + firebase - auth, firestore

---
+++ src/firebase/config.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyDdMqPkaEWmXJVTSUXqTgqaW4KhVR1WKI4',
+    authDomain: 'react-tokiotask-app.firebaseapp.com',
+    projectId: 'react-tokiotask-app',
+    storageBucket: 'react-tokiotask-app.firebasestorage.app',
+    messagingSenderId: '334607496302',
+    appId: '1:334607496302:web:23bb31a276a093d9b7a27f',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app)
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app)
+
+export { app, db, auth }
